b407c53f1b7d831011f36126a7039f9a8480e272
SrdjanNikolin <srdjannikolin@yahoo.com>
2020-01-13 14:29:13 +0100

    Migrated project to .net core 3.1 version.

--- Startup.cs
+++ Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace GameCRUDApp
 {
@@ -17,20 +18,27 @@ namespace GameCRUDApp
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddRazorPages();
+            services.AddControllersWithViews();
             services.AddSingleton<IGameService, GameService>();
             services.AddSingleton<IGameRepository, GameRepository>();
         }
 
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
             app.UseStaticFiles();
+            app.UseEndpoints(endpoints => 
+                endpoints.MapDefaultControllerRoute()
+            );
 
-            app.UseMvc(route => route.MapRoute("default", "{controller=Home}/{action=Index}/{id?}"));
+            
         }
     }
 }
